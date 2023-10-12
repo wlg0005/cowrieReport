ec6ea05a736e0c6f648f77ca9eeb7c97acddc2d2
Will Green <will@willgreen.tech>
2023-10-12 16:25:11 -0500

    Code rework to golang, implemented deduplication

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.3
+
+require github.com/adrg/strutil v0.3.1
---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/adrg/strutil"
+	"github.com/adrg/strutil/metrics"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Panic(e)
+	}
+}
+
+func getEntries(fname string) []map[string]interface{} {
+	logFile, err := os.Open(fname)
+	check(err)
+	defer logFile.Close()
+
+	var entries []map[string]interface{}
+
+	scanner := bufio.NewScanner(logFile)
+	for scanner.Scan() {
+		var entry = parseJson(scanner.Text())
+		entries = append(entries, entry)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Panic(err)
+	}
+
+	return entries
+}
+
+func parseJson(entry string) map[string]interface{} {
+	var result map[string]interface{}
+	json.Unmarshal([]byte(entry), &result)
+
+	return result
+}
+
+func removeSlices(entries []map[string]interface{}) []map[string]interface{} {
+	var new_entries []map[string]interface{}
+
+	for _, e := range entries {
+		message := e["message"]
+		if message == nil {
+			continue
+		}
+
+		if reflect.ValueOf(message).Kind() != reflect.Slice {
+			new_entries = append(new_entries, e)
+		}
+	}
+
+	return new_entries
+}
+
+func getSessions(entries []map[string]interface{}) []string {
+	var sessions []string
+	for _, e := range entries {
+		if e["eventid"] == "cowrie.login.success" {
+			sessions = append(sessions, e["session"].(string))
+		}
+
+	}
+
+	return sessions
+}
+
+func getAllCommands(entries []map[string]interface{}, sessions []string) []string {
+	var allCommands []string
+	for _, s := range sessions {
+		commands := ""
+		for _, e := range entries {
+			if e["session"] == s {
+				if strings.Contains(e["message"].(string), "CMD") {
+					message := strings.Replace(e["message"].(string), "CMD: ", "", 1)
+					commands += message + "\n"
+				}
+			}
+		}
+
+		if commands == "" {
+			continue
+		}
+
+		allCommands = append(allCommands, commands)
+	}
+
+	return allCommands
+}
+
+func getUniqueCommands(allCommands []string, ratio float64) []string {
+	var uniqueCommands []string
+	uniqueCommands = append(uniqueCommands, "0")
+	for _, ac := range allCommands {
+		var ratios []float64
+		for _, uc := range uniqueCommands {
+			similarity := strutil.Similarity(uc, ac, metrics.NewHamming())
+			ratios = append(ratios, similarity)
+		}
+
+		if slices.Max(ratios) > ratio {
+			continue
+		}
+
+		uniqueCommands = append(uniqueCommands, ac)
+	}
+
+	return uniqueCommands
+}
+
+func main() {
+
+	var fname string
+	flag.StringVar(&fname, "f", "", "Specify cowrie json log file")
+
+	var output string
+	flag.StringVar(&output, "o", "", "Specify output location for report (Default: ./report_[filename])")
+
+	var ratio string
+	flag.StringVar(&ratio, "r", "0.1", "Specify similarity ratio as a float (Default: 0.1)")
+
+	flag.Parse()
+
+	if fname == "" {
+		fmt.Println("Please specify -f. Use --help for more information.")
+		os.Exit(2)
+	}
+
+	r, err := strconv.ParseFloat(ratio, 8)
+	check(err)
+
+	entries := removeSlices(getEntries(fname)) // process all json entries in log
+	sessions := getSessions(entries)           // get all cowrie.login.success sessions
+	allCommands := getAllCommands(entries, sessions)
+	uniqueCommands := getUniqueCommands(allCommands, r)
+
+	var f *os.File
+	if output == "" {
+		f, err = os.Create("./report_" + filepath.Base(fname))
+		check(err)
+		defer f.Close()
+	} else {
+		f, err = os.Create(output)
+		check(err)
+		defer f.Close()
+	}
+
+	for _, c := range uniqueCommands[1:] {
+		f.WriteString(strings.Repeat("-", 50) + "\n")
+		_, err = f.WriteString(strings.TrimSuffix(c, "\n") + "\n")
+		check(err)
+		f.WriteString(strings.Repeat("-", 50) + "\n\n")
+	}
+}
